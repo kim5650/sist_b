74fafd010492db07d5fd79c2410b18c96cb1a2c5
Your NameKimYEONSUASDADWQCEQWCEQEQCECQWE <rladustn50@nate.com>
2023-07-24 15:24:51 +0900

    first commit

---
+++ st_b/src/main/java/com/example/demo/controller/DeptController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.dao.DeptDAO_JPA;
+import com.example.demo.dao.DeptDAO_MB;
+
+import lombok.Setter;
+
+@Controller
+@Setter
+public class DeptController {
+		
+	
+		@Autowired
+		private DeptDAO_JPA dao_jpa;
+		
+		@Autowired
+		private DeptDAO_MB dao_mb;
+	
+		@GetMapping("/dept/list")
+		public void list(Model model) {
+			model.addAttribute("list",dao_jpa.findAll());
+			
+			
+		}
+		
+		@GetMapping("/dept/detail/{:dno}")
+		public String detail(@PathVariable("dno") int dno, Model model) {
+			model.addAttribute("d",dao_mb.findByDno(dno));
+		
+				return "/dept/detail";
+			
+		}
+		
+}
