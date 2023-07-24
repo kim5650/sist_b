dc81a600c8af0fa5375af29bfa426b950cb72c48
Your NameKimYEONSUASDADWQCEQWCEQEQCECQWE <rladustn50@nate.com>
2023-07-24 11:19:44 +0900

    login finish

---
+++ LoginTest.java
@@ -0,0 +1,15 @@
+class  LoginTest
+{
+	public boolean isMember(String id, String pwd){
+		
+		if(id.equals("tiger" && pwd.equals("1234"){
+			return true;
+
+		}else{
+			
+			return false;
+
+		}
+
+	}
+}
