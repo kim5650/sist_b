857dd14527b5f7e0bdbf6872bc2f69ac6bc012f8
Your NameKimYEONSUASDADWQCEQWCEQEQCECQWE <rladustn50@nate.com>
2023-07-24 16:47:03 +0900

    부서수정 진행이요~(기면수)

--- st_b/src/main/java/com/example/demo/controller/DeptController.java
+++ st_b/src/main/java/com/example/demo/controller/DeptController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.DeptDAO_JPA;
@@ -23,6 +24,14 @@ public class DeptController {
 		
 		@Autowired
 		private DeptDAO_MB dao_mb;
+		
+		@RequestMapping("updateDept")
+		public ModelAndView update() {
+			ModelAndView mav = new ModelAndView("/dept/update");
+			return mav;
+			
+		}
+		
 	
 		@GetMapping("/dept/list")
 		public void list(Model model) {
